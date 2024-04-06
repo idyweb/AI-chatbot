aa9ad92a38cf7cda3da49966cf556b7ac6209d92
Idongesit Inyang <inyangidongesit22@gmail.com>
2024-04-06 15:58:27 +0100

    revert

--- chatbot.py
+++ chatbot.py
@@ -1,88 +1,21 @@
-# import random
-# import json
-# import pickle
-# import numpy as np
-# import nltk
-# import requests
-# import threading
-
-# from nltk.stem import WordNetLemmatizer
-# from keras.models import load_model
-
-
-# lemmatizer = WordNetLemmatizer()
-
-# # Download NLTK data
-# nltk.download('punkt')
-# nltk.download('wordnet')
-
-
-# intents = json.loads(open('intents.json').read())
-# words = pickle.load(open('words.pkl', 'rb'))
-# classes = pickle.load(open('classes.pkl', 'rb'))
-
-# model = load_model('chatbot_model.h5')
-
-# def clean_up_sentence(sentence):
-#     sentence_words = nltk.word_tokenize(sentence)
-#     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-#     return sentence_words
-
-
-# def bag_of_words(sentence):
-#     sentence_words = clean_up_sentence(sentence)
-#     bag = [0] * len(words)
-#     for w in sentence_words:
-#         for i, word in enumerate(words):
-#             if word == w:
-#                 bag[i] = 1
-#     return np.array(bag)
-
-# def predict_class(sentence):
-#     bow = bag_of_words(sentence)
-#     res = model.predict(np.array([bow]))[0]
-#     ERROR_THRESHOLD = 0.25
-#     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-    
-#     results.sort(key=lambda x: x[1], reverse=True)
-#     return_list = []
-#     for r in results:
-#         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
-#     return return_list
-
-# def get_response(intents_list, intents_json):
-#     tag = intents_list[0]['intent']
-#     list_of_intents = intents_json['intents']
-#     for i in list_of_intents:
-#         if i['tag'] == tag:
-#             result = random.choice(i['responses'])
-#             break
-#     return result
-
-
-# def run_chatbot():
-#     print("Go! Bot is running!")
-
-#     while True:
-#         message = input("")
-#         intents_list = predict_class(message)
-#         result = get_response(intents_list, intents)
-#         print(result)
-
 import random
 import json
 import pickle
 import numpy as np
 import nltk
+import requests
 import threading
+
 from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
+
 
 lemmatizer = WordNetLemmatizer()
 
 # Download NLTK data
 nltk.download('punkt')
 nltk.download('wordnet')
+
 
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
@@ -92,41 +25,46 @@ model = load_model('chatbot_model.h5')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = {lemmatizer.lemmatize(word.lower()) for word in sentence_words}
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
+
 
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
-    bag = [1 if word in sentence_words else 0 for word in words]
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
     return np.array(bag)
 
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [(i, r) for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    
     results.sort(key=lambda x: x[1], reverse=True)
-    return [{'intent': classes[r[0]], 'probability': str(r[1])} for r in results]
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
 
 def get_response(intents_list, intents_json):
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if i['tag'] == tag:
-            return random.choice(i['responses'])
-    return None
+            result = random.choice(i['responses'])
+            break
+    return result
 
-def process_message(message):
-    intents_list = predict_class(message)
-    return get_response(intents_list, intents)
 
 def run_chatbot():
     print("Go! Bot is running!")
 
     while True:
         message = input("")
-        result = process_message(message)
+        intents_list = predict_class(message)
+        result = get_response(intents_list, intents)
         print(result)
-
-if __name__ == "__main__":
-    run_chatbot()
