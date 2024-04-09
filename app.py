7cde439e8720da3174af0f2ac92eca82dda6e0e3
Idongesit Inyang <inyangidongesit22@gmail.com>
2024-04-09 18:43:22 +0100

    formatted the response

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from chatbot import predict_class, get_response, intents
+import markdown
 
 app = Flask(__name__)
 
@@ -12,6 +13,10 @@ def predict():
     text = request.get_json().get("message")
     intents_list = predict_class(text)
     response = get_response(intents_list, intents)
+    # Convert Markdown table to HTML table
+    response = markdown.markdown(response, extensions=['tables'])
+     # Convert \n to <br> for HTML display
+    response = response.replace('\n', '<br>')
     message = {"answer":response}
     return jsonify(message)
 
