4079deaf9dd4b09bf18aef37013f9e7b40b75a1e
Idongesit Inyang <inyangidongesit22@gmail.com>
2024-04-14 17:56:08 +0100

    increased training epoch

--- train.py
+++ train.py
@@ -63,7 +63,7 @@ model.add(tf.keras.layers.Dense(len(trainY[0]), activation='softmax'))
 sgd = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-hist = model.fit(np.array(trainX), np.array(trainY), epochs=150, batch_size=5, verbose=1)
+hist = model.fit(np.array(trainX), np.array(trainY), epochs=200, batch_size=5, verbose=1)
 model.save('model/chatbot_model.h5', hist)
 print('Done')
 
